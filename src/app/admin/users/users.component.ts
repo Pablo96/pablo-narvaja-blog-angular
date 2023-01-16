d57b3f28bef9c137673f7a035b0b7c3f45abfe79
pablo96n <pnarvaja.21@gmail.com>
2023-01-16 20:14:47 -0300

    users wip

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/shared/user.model';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  active_user: User = new User();
 
+  ngOnInit() {
+    this.active_user.active = true;
+    this.active_user.description = "I am a \nprogramer\nxdxd";
+    this.active_user.last_name = "Roberts";
+    this.active_user.first_name = "Pablo";
+    this.active_user.email = "pablo.roberts@gmail.com";
+    this.active_user.profile_pic_url = "assets/img/profile_pic.png";
+    this.active_user.superuser = true;
+  }
 }
---
+++ src/app/shared/user.model.ts
@@ -0,0 +1,9 @@
+export class User {
+  email: string;
+  profile_pic_url: string;
+  first_name: string;
+  last_name: string;
+  description: string;
+  active: boolean;
+  superuser: boolean;
+}
