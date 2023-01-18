def253b5629b1827f607fb4c019fe2dbeb7efb41
pablo96n <pnarvaja.21@gmail.com>
2023-01-17 21:42:51 -0300

    image resources

--- src/app/admin/resources/resources.component.ts
+++ src/app/admin/resources/resources.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./resources.component.css']
 })
 export class ResourcesComponent {
-
+  resource_info: string = "size: 15mb<br>extension: PNG (Portable Network Graphics)<br>width: 480px<br>height: 480px<br>bits per pixel: 24 (r:8, g:8, b:8)<br>indexed colors: false"
 }
